efeffde1cb952e96a77a3081270d39864b8dd648
Matt Parker <mattmparker@gmail.com>
2013-08-26 13:40:19 -0600

    Expand the report, tweak the report labeling.

--- generate_report.r
+++ generate_report.r
@@ -17,16 +17,46 @@ qtr_label <- "2013 Q2"
 
 # Loop over each county (or collection of counties)
 counties <- list(
-    data.frame(label = "Denver County",
-               county = "Denver"),
-    data.frame(label = "Boulder County",
-               county= "Boulder"),
-    data.frame(label = "Broomfield County",
-               county= "Broomfield"),
-    data.frame(label = "Jefferson County",
-               county= "Jefferson"),
-    data.frame(label = "Tri-County Health Department",
-               county= c("Adams", "Arapahoe", "Douglas"))
+    data.frame(county = "Denver",
+               hd_label = "Denver County",
+               county_label = "Denver County"),
+               
+    data.frame(county = "Boulder",
+               hd_label = "Boulder County",
+               county_label = "Boulder County"),
+               
+    data.frame(county = "Broomfield",
+               hd_label = "Broomfield County",
+               county_label = "Broomfield County"),
+               
+    data.frame(county = "Jefferson",
+               hd_label = "Jefferson County",
+               county_label = "Jefferson County"),
+               
+    data.frame(county = "Adams",
+               hd_label = "Adams County",
+               county_label = "Adams County"),
+               
+    data.frame(county = "Arapahoe",
+               hd_label = "Arapahoe County",
+               county_label = "Arapahoe County"),
+               
+    data.frame(county = "Douglas",
+               hd_label = "Douglas County",
+               county_label = "Douglas County"),
+               
+    data.frame(county = c("Adams", "Arapahoe", "Douglas"),
+               hd_label = "Tri-County Health Department",
+               county_label = "Adams, Arapahoe, and Douglas Counties"),
+
+    data.frame(county = c("Adams", "Arapahoe", "Boulder", "Broomfield",
+                          "Denver", "Douglas", "Jefferson"),
+               hd_label = "the Denver Metropolitan Area",
+               county_label = "the Denver Metropolitan Area")
+
+
+               
+               
 )
 
 
@@ -37,7 +67,7 @@ lapply(counties, function(x) {
     mdfile <- file.path("temp", "county_report.md")
 
     htmlfile <- file.path("reports", 
-                          paste(x$label[1], 
+                          paste(x$hd_label[1], 
                                 " ",
                                 qtr_label, 
                                 ".html",
