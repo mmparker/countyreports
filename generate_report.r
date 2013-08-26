810d0ce2bb779e011b67347bd263437c86db349f
Matt Parker <mattmparker@gmail.com>
2013-08-26 13:22:11 -0600

    Iterate over each county or group of counties
    It works!

--- generate_report.r
+++ generate_report.r
@@ -10,12 +10,50 @@ options(stringsAsFactors = FALSE)
 library(knitr)
 library(markdown)
 
-# Knit it
-knit("county_report.rmd")
 
-markdownToHTML(file = "county_report.md",
-               output = "county_report.html",
-               stylesheet = file.path("..", "..", "css", "tb_report.css"))
+# Temp quarter label
+qtr_label <- "2013 Q2"
 
 
+# Loop over each county (or collection of counties)
+counties <- list(
+    data.frame(label = "Denver County",
+               county = "Denver"),
+    data.frame(label = "Boulder County",
+               county= "Boulder"),
+    data.frame(label = "Broomfield County",
+               county= "Broomfield"),
+    data.frame(label = "Jefferson County",
+               county= "Jefferson"),
+    data.frame(label = "Tri-County Health Department",
+               county= c("Adams", "Arapahoe", "Douglas"))
+)
 
+
+# Loop over the counties
+lapply(counties, function(x) {
+
+
+    mdfile <- file.path("temp", "county_report.md")
+
+    htmlfile <- file.path("reports", 
+                          paste(x$label[1], 
+                                " ",
+                                qtr_label, 
+                                ".html",
+                                sep = "")
+    )
+
+
+
+    # Knit it
+    knit("county_report.rmd", 
+         output = mdfile)
+
+    # Convert to HTML
+    markdownToHTML(file = mdfile,
+                   output = htmlfile,
+                   stylesheet = file.path("..", "css", "tb_report.css"))
+
+
+})
