aef9ed33183356420e6fad219c952464e634f12c
Matt Parker <mattmparker@gmail.com>
2013-09-03 12:53:39 -0600

    Another label variant.

--- generate_report.r
+++ generate_report.r
@@ -19,47 +19,56 @@ qtr_label <- "2013 Q2"
 counties <- list(
     data.frame(county = "Denver",
                hd_label = "Denver County",
+               hd_tag = "Denver",
                county_label = "Denver County",
                prefix = "denver_"),
 
     data.frame(county = "Boulder",
                hd_label = "Boulder County",
+               hd_tag = "Boulder",
                county_label = "Boulder County",
                prefix = "boulder_"),
 
     data.frame(county = "Broomfield",
                hd_label = "Broomfield County",
+               hd_tag = "Broomfield",
                county_label = "Broomfield County",
                prefix = "broomfield_"),
 
     data.frame(county = "Jefferson",
                hd_label = "Jefferson County",
+               hd_tag = "Jefferson",
                county_label = "Jefferson County",
                prefix = "jefferson_"),
 
     data.frame(county = "Adams",
                hd_label = "Adams County",
+               hd_tag = "Adams",
                county_label = "Adams County",
                prefix = "adams_"),
 
     data.frame(county = "Arapahoe",
                hd_label = "Arapahoe County",
+               hd_tag = "Arapahoe",
                county_label = "Arapahoe County",
                prefix = "arapahoe_"),
 
     data.frame(county = "Douglas",
                hd_label = "Douglas County",
+               hd_tag = "Douglas",
                county_label = "Douglas County",
                prefix = "douglas_"),
 
     data.frame(county = c("Adams", "Arapahoe", "Douglas"),
                hd_label = "Tri-County Health Department",
+               hd_tag = "Tri-County",
                county_label = "Adams, Arapahoe, and Douglas Counties",
                prefix = "tricounty_"),
 
     data.frame(county = c("Adams", "Arapahoe", "Boulder", "Broomfield",
                           "Denver", "Douglas", "Jefferson"),
                hd_label = "the Denver Metropolitan Area",
+               hd_tag = "Denver Metro",
                county_label = "the Denver Metropolitan Area",
                prefix = "metro_")
 
